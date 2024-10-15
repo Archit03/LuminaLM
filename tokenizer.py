6dcc60e911b30e4b1f404c1bf4ab82f01243ee40
Archit03 <architsood99@hotmail.com>
2024-10-15 14:29:07 +0530

    tokenizer update-2.

--- tokenizer.py
+++ tokenizer.py
@@ -50,7 +50,8 @@ def load_tokenizer(path="bpe_token.json"):
     return Tokenizer.from_file(path)
 
 # Example Usage
-directory = r'C:\Users\LENOVO\Desktop\Sentient-Sculptor-LLM\Data'  # Replace with the directory path containing your .txt and .csv files
-tokenizer, trainer = create_tokens(vocab_size=199997)
-train_tokenizer(tokenizer, trainer, directory)
-save_tokenizer(tokenizer)
+if __name__ == "__main__":
+    directory = r'C:/Users/LENOVO/Desktop/Sentient-Sculptor-LLM/Data'  # Adjust to your actual directory path
+    tokenizer, trainer = create_tokens(vocab_size=199997)
+    train_tokenizer(tokenizer, trainer, directory)
+    save_tokenizer(tokenizer)
